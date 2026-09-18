f5118e03d3b577e79156764d6cb6eb2637076994
The MagicIAn <62627744+TheMagicIAn-creator@users.noreply.github.com>
2026-09-18 12:53:49 +0000

    feat: preenchimento de arquivos e comentários

---
+++ main.py
@@ -0,0 +1,10 @@
+from modulo_01.processamento import processar_dados
+
+# Chama do arquivo "processamento.py" da pasta modulo_01 a
+# função definida "processar_dados"
+
+def exibir_tela():
+    print("=" * 40)
+    print("      SISTEMA DE PORTFÓLIO TOPZERA      ")
+    print("=" * 40)
+    
---
+++ modulo_01/processamento.py
@@ -0,0 +1,3 @@
+def processar_dados(nome): # Função definida chamada em main.py
+    """Simula o processamento de uma informação."""
+    return f"Dados de '{nome}' processados com sucesso!"
